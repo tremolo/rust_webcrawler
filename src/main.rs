852a631068160d6ee8a01f385f76af7d92d754a8
Lutz Paelike <lutz@delutze.de>
2015-09-16 20:05:54 +0200

    read from url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate hyper;
+use hyper::client::Client;
+use std::io::Read;
+
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+
+    let res = client.get("https://www.heise.de").send();
+
+
+    let mut response = match res {
+            Ok(x)  => x,
+            Err(err) =>  panic!("{:?}", err)
+    };
+
+    let mut body = String::new();
+    response.read_to_string(&mut body);
+    println!("{}", body);
+
 }
